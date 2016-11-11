fc8d32a262613318fe3b88e968f4543f40b14ed7
Sonya Panich <sonyapanich@gmail.com>
2016-11-11 12:07:40 -0800

    yay for new bugs

--- server/controllers/cards-controller.js
+++ server/controllers/cards-controller.js
@@ -1,20 +1,29 @@
-var Card = require('../models/card');
+var Card = require("../models/card");
 
 module.exports.create = function(req, res){
     var card = new Card(req.body);
     card.save(function(err, result){
         res.json(result);
     });
-}
+};
 
 // module.exports.list = function(req,res){
 //     Card.find({}, function(err, results){
 //         res.json(results);
 //     })
 // }
+
 module.exports.chargeCard = function(req,res){
     var user = req.body.user_name;
     var charge = req.body.charge;
-    if Card.findOne
-}
-// "http://mongoosejs.com/docs/queries.html"
+
+    Card.find({user_name: user}, function (err, user) {
+        card = card[0];
+        card.balance += charge;
+
+        card.save(function (err) {
+            if(err) {
+                console.error("ERROR!");
+            }
+        });
+    });
